234ff0c919516fb52f4af11007185e399642c3bd
Nikola Cerovski <nikola.cerovski@gmail.com>
2017-05-15 18:04:42 +0200

    c

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="{{ route('sendmail') }}" method="post">
+    <form action="{{ ../sendmail }}" method="post">
       {{ csrf_field() }}
       <input type="email" name="mail" placeholder="email address">
       <input type="text" name="title" placeholder="title">
